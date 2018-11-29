5df057658122024d34815386cd3245926721a892
Yash Narendra Saraf <yash21saraf@gmail.com>
2018-11-29 17:48:10 -0500

    Updated CORS

--- app.py
+++ app.py
@@ -1,74 +1,40 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response, current_app
+from flask_cors import CORS, cross_origin
 from flask import Response
-app = Flask(__name__)
+import json
+import urllib.request, urllib.error
+import urllib
+import re
 
-pub_to_topics = {}
-topics_to_subscribers = {'Movies':[], 'Sports':[], 'World': [], 'Politics':[]}
-topics_to_content = {'Movies': [], 'Sports':[], 'World':[], 'Politics':[]}
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:5000"}})
+# CORS(app, resources={r"/api/*": {"origins" : "*"}})
 
 @app.route('/')
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def index():
 	return render_template('form.html')
 
-@app.route('/selectPubSub', methods=['POST'])
-def selectPubSub():
-	pubsubtype = request.form['pubsubtype']
-	topic = request.form['topic']
-	userid = request.form['userid']
+@app.route('/selectsearch', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def selectsearch():
+	query = request.form['search_text']
+	print(query)
+	query = urllib.parse.quote_plus(query)
+	url = 'http://localhost:8983/solr/vsm/select?indent=on&q='+ query + '&wt=json'
+	data = urllib.request.urlopen(url)
+	docs = json.load(data)['response']['docs']
+	print(str(docs))
+	return jsonify(docs)
 
-	# print(userid)
-	if pubsubtype == 'Publisher':
-		pub_to_topics[userid] = topic
-	else:
-		value = topics_to_subscribers[topic]
-		if userid not in value:
-			topics_to_subscribers[topic].append(userid)
-	print(pub_to_topics)
-	print(topics_to_subscribers)
-	if pubsubtype and topic:
-		return jsonify({'pubsubtype' : pubsubtype})
-
-	return jsonify({'error' : 'Missing data!'})
 
 @app.route('/publisher', methods=['POST'])
 def publisher():
 	content = request.form['content']
 	topic = request.form['topic_2']
-	topics_to_content[topic].append(content)
-	print(topics_to_content)
-	if content:
-		return jsonify({'content' : content})
-
 	return jsonify({'error' : 'Missing data!'})
 
-
-@app.route("/stream")
-def stream():
-	# messages = "abcwddwd"
-	# previous_messages = "ab"
-	# print('here124')
-	# def eventStream():
-	# 	print('there1')
-	# 	messages = "abcwddwd"
-	# 	previous_messages = "ab"
-	# 	while True:
-	# 		# Pull data from the database
-	# 		# and see if there's a new message
-	# 		if len(messages) > len(previous_messages):
-	# 			print("data: {}\n\n".format(messages[len(messages)-1]))
-	# 			yield "data: {}\n\n".format(messages[len(messages)-1])
-	userid = request.args['userid']
-	print(userid)
-	posts_to_be_published = 'data: '
-	# posts_to_be_published += '\n'
-	for key, value in topics_to_subscribers.items():
-		if userid in value:
-			posts_to_be_published += 'Topic Name: ' + key + ':' + '  '
-			posts_to_be_published += str(topics_to_content[key])
-			# posts_to_be_published += '\n'
-	posts_to_be_published+='\n\n'
-	print(posts_to_be_published)
-	return Response(posts_to_be_published, mimetype="text/event-stream")
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
--- static/js/form.js
+++ static/js/form.js
@@ -1,126 +1,54 @@
-var headers = {};
+function descriptiveFunctionName() {
 
-var merchantDetails = {
-  "BestBuy" : {
-    "pantheonMerchantName": "bestbuy-test",
-    "merchantId": "598740a8-ba1f-4366-a3de-73ebef1acbfc",
-    "merchantApiKey": "46997a64fa11c82597bf86f53e58ba13",
-    "merchantApiSecret": "2f4199f3b5d6bbb73cc406804b4cffd95c5a80a8da8ade13c320a1775c708ebf"
-  },
-  "Walmart" : {
-    "pantheonMerchantName": "walmart-test",
-    "merchantId": "6d69860d-5de8-44fb-9124-fd631fefa979",
-    "merchantApiKey": "478791a5c24f4c92eb8fa252032aae7a",
-    "merchantApiSecret": "90a8bf8b7d62bece7d0fae19b4a38c6dd4d6df30f6a2d093fb7f87d23e3c87a"
-  }
-};
-
-var zbMerchantId, zbMerchantApiKey, zbMerchantApiSecret, zbMerchantName;
-var merchantName, productsToFetch, offersToCreate, maxItemPrice;
-
-function setOmnywayMerchantDetails(pantheonMerchantId, pantheonMerchantApiKey, pantheonMerchantApiSecret, pantheonMerchantName){
-  zbMerchantId = pantheonMerchantId;
-  zbMerchantApiKey = pantheonMerchantApiKey;
-  zbMerchantApiSecret = pantheonMerchantApiSecret;
-  zbMerchantName = pantheonMerchantName;
-};
-
-function makeAjaxCall(callType, apiPath, handleSuccess, handleError, payload) {
-  
-  if (callType.toUpperCase() == "POST") {
+  $('#form2').on('submit', function(event) {
+  console.log("form 2 executed") ;
     $.ajax({
-           url: apiPath,
-           type: "POST",
-           headers: headers,
-           data: JSON.stringify(payload),
-           dataType: "jsonp",
-           contentType: "application/json",
-           processData: false,
-           success: handleSuccess,
-           error: handleError,
-		   useCors: false
-		   
-           });
-  } else if (callType.toUpperCase() == "GET") {
-    $.ajax({
-           url: apiPath,
-           type: "GET",
-           headers: headers,
-           success: handleSuccess,
-           error: handleError,
-		   useCors: false
-           });
-  }
+      data : {
+        content : $('#content').val(),
+          topic_2 : $('#topic_2 option:selected').val()
+      },
+      type : 'POST',
+      url : '/publisher'
+    })
+    .done(function(data) {
+
+      if (data.error) {
+        $('#errorAlert').text(data.error).show();
+        $('#successAlert').hide();
+      }
+      else {
+        $('#successAlert').text(data.content).show();
+        $('#errorAlert').hide();
+      }
+
+    });
+
+    event.preventDefault();
+
+  });
+}
+var id = -1;
+function anotherDescriptiveFunctionName() {
+
+  $('#form1').on('submit', function(event) {
+  console.log("form1 executed") ;
+		$.ajax({
+			data : {
+                search_text: $('#search_text').val()
+			},
+			type : 'POST',
+			url : '/selectsearch'
+		})
+		.done(function(data) {
+        
+		});
+
+		event.preventDefault();
+
+	});
 }
 
 $(document).ready(function() {
-	
+    descriptiveFunctionName();
+    anotherDescriptiveFunctionName();
 });
-
-function displayMerchantDetails(merchantObj) {
-  $("#merchant_name").val(merchantObj.pantheonMerchantName);
-  $("#merchant_id").val(merchantObj.merchantId);
-  $("#merchant_api_key").val(merchantObj.merchantApiKey);
-  $("#merchant_api_secret").val(merchantObj.merchantApiSecret);
-}
-
-function search() {
-  // document.getElementById("fetchBtn").disabled = true;
-  searchText = document.getElementById("search_text").value;
-  console.log("called -> " + searchText);
-
-  if (searchText != null) {
-	makeAjaxCall( 'GET', 
-                    'http://localhost:8983/solr/VSM/select?indent=on&q=syria&wt=json', 
-                    function (data) {
-                      console.log('Search Text: ' + data);
-                    }, 
-                    function (error) {
-                      console.log('Something went wrong: ' + error);
-                    }
-                  );
-  } else {
-    alert("Empty Search Text");
-  }
-}
-
-function downloadOfferCSV() {
-  if($("#resultant").text() == "") {
-    alert("No offers to download...");
-    return;
-  }
-
-  let offerList = $("#resultant").text().replace(/<br id>/g, "\r\n");
-  download('offer-list.csv', offerList);
-}
-
-function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-  $("#downloadOfferBtn").attr("disabled", "true");
-}
-
-function addCatalogItems() {
-  const skuCount = catalogItems.length;
-  let itemIndex = 0;
-  $("#uploadCatalogBtn").attr("disabled", "true");
-  catalogItems.forEach((element, index) => {
-      makeAjaxCall( 'POST', 
-                    'http://localhost:8983/solr/VSM/select?indent=on&q=syria&wt=json', 
-                    function (data) {
-                      console.log('product uploaded: ' + data);
-                    }, 
-                    function (error) {
-                      console.log('product upload FAILED: ' + error);
-                      // alert('Something went wrong: ' + error);
-                    }
-                  );
-  });
-  $('#resultant').text(zbOffersText);
-  $("#downloadOfferBtn").prop("disabled", "");
-}
