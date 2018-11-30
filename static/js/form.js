f2b9a50667eb800e93e48fbc914c05343cc5aad4
MikiP26 <miki.padhiary@talentica.com>
2018-11-30 11:01:48 -0500

    Added city and language filter

--- static/js/form.js
+++ static/js/form.js
@@ -82,10 +82,41 @@ function returnSearchResults() {
 			}
 			$("#tweets-div").html(htmlStr);
 			$("#total-tweets").html("Tweets returned " +no_of_tweets);
+			$("#chartContainer").getJSON("https://canvasjs.com/data/gallery/javascript/daily-sales-data.json", addData);
+			addData();
 		}
 	});
 }
 
+
+var dataPoints = [];
+
+var chart = new CanvasJS.Chart("#chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Daily Sales Data"
+	},
+	axisY: {
+		title: "Units",
+		titleFontSize: 24
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,### Units",
+		dataPoints: dataPoints
+	}]
+});
+
+function addData(data) {
+	for (var i = 0; i < data.length; i++) {
+		dataPoints.push({
+			x: new Date(data[i].date),
+			y: data[i].units
+		});
+	}
+	chart.render();
+}
 
 
 $(document).ready(function() {
